20e3e4649014be82342ebfcc3e62c7b10cca24ef
Ashish Mishra <ashishmishra9467@gmail.com>
2023-07-21 13:52:39 +0530

    project day-2

---
+++ src/Routers/Router.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from '../Pages/Home'
+import Product from '../Pages/Product'
+import Productdetails from '../Pages/Productdetails'
+import Cart from '../Pages/Cart'
+import Login from '../Pages/Login'
+import Signup from '../Pages/Signup'
+import Checkout from '../Pages/Checkout'
+
+const Router = () => {
+  return (
+    <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="product" element={<Product/>}/>
+        <Route path="product/:id" element={<Productdetails/>}/>
+        <Route path="cart" element={<Cart/>}/>
+        <Route path="login" element={<Login/>}/>
+        <Route path="signup" element={<Signup/>}/>
+        <Route path="checkout" element={<Checkout/>}/>
+    </Routes>
+  )
+}
+
+export default Router
